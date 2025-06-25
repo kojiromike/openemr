87d6e3b2516d8e763f35a17a054051f148af585f
Michael A. Smith <michael@smith-li.com>
2025-06-25 16:13:03 -0400

    refactor(InstallerAuto): rewrite as a Symfony CLI command

--- contrib/util/installScripts/InstallerAuto.php
+++ contrib/util/installScripts/InstallerAuto.php
@@ -4,68 +4,48 @@
  * This script is for automatic installation and configuration
  *   of OpenEMR.
  *
- * This script is meant to be run as php command line (php-cli),
- *   and needs to be first activated by removing the 'exit' line
- *   at top (via sed command).
+ * This script uses Symfony Console component to provide a more
+ * structured and maintainable CLI interface for OpenEMR installation.
  *
- * To activate script, need to comment out the exit command at top
- *   of script.
+ * Usage:
+ *   php InstallerAuto.php [options]
  *
- * Command ( Note that the ordering and number of custom settings
- *           that can be sent is flexible ):
- *     php -f iuser=[iuser] iuname=[iuname] iuserpass=[iuserpass] igroup=[igroup]
- *       server=[server] loginhost=[loginhost] port=[port] root=[root] rootpass=[rootpass]
- *       login=[login] pass=[pass] dbname=[dbname] collate=[collate] site=[site]
- *       source_site_id=[source_site_id] clone_database=[clone_database]
+ *   Options:
+ *     --iuser=VALUE            Initial user login name (default: admin)
+ *     --iuname=VALUE           Initial user last name (default: Administrator)
+ *     --iuserpass=VALUE        Initial user password (default: pass)
+ *     --igroup=VALUE           Practice group name (default: Default)
+ *     --server=VALUE           MySQL server (default: localhost)
+ *     --loginhost=VALUE        PHP/Apache server (default: localhost)
+ *     --port=VALUE             MySQL port (default: 3306)
+ *     --root=VALUE             MySQL server root username (default: root)
+ *     --rootpass=VALUE         MySQL server root password (default: empty)
+ *     --login=VALUE            Username to MySQL openemr database (default: openemr)
+ *     --pass=VALUE             Password to MySQL openemr database (default: openemr)
+ *     --dbname=VALUE           MySQL openemr database name (default: openemr)
+ *     --collate=VALUE          Collation for MySQL (default: utf8mb4_general_ci)
+ *     --site=VALUE             Location of this instance in sites/ (default: default)
+ *     --source_site_id=VALUE   Location of instance to clone and mirror
+ *     --clone_database=VALUE   If set, will clone database from source_site_id
+ *     --no_root_db_access=VALUE Use pre-created database and user credentials
+ *     --development_translations=VALUE Use development translations
  *
- *   Description of settings (default value in parenthesis):
- *     iuser      -> initial user login name (admin)
- *     iuname     -> initial user last name (Administrator)
- *     iuserpass  -> initial user password (pass)
- *     igroup     -> practice group name (Default)
- *     server     -> mysql server (localhost)
- *     loginhost  -> php/apache server (localhost)
- *     port       -> MySQL port (3306)
- *     root       -> MySQL server root username (root)
- *     rootpass   -> MySQL server root password ()
- *     login      -> username to MySQL openemr database (openemr)
- *     pass       -> password to MySQL openemr database (openemr)
- *     dbname     -> MySQL openemr database name (openemr)
- *     collate    -> collation for mysql (utf8_general_ci)
- *     site       -> location of this instance in sites/ (default)
- *     source_site_id -> location of instance to clone and mirror ()
- *                         Advanced option of multi site module to allow cloning/mirroring of another local site.
- *     clone_database -> if set to anything, then will clone database from source_site_id ()
- *                         Advanced option of multi site module to allow cloning/mirroring of another local database.
- *     no_root_db_access -> if set to anything, will use pre-created and pre-configured login/pass/dbname and
- *                             will disable cloning / migration since that generally requires root access to the db
- *     development_translations -> If set to anything, will then download and use the development set (updated daily)
- *                                   of translations (indirectly) from the github repository.
- *
- *     Examples of use:
- *     1) Install using default configuration settings
- *          php -f InstallerAuto.php
- *     2) Provide root sql user password for installation
- *        (otherwise use default configuration settings)
- *          php -f InstallerAuto.php rootpass=howdy
- *     3) Provide root sql user password and openemr sql user password
- *        (otherwise use default configuration settings)
- *          php -f InstallerAuto.php rootpass=howdy pass=hey
- *     4) Provide sql user settings and openemr user settings
- *        (otherwise use default configuration settings)
- *          php -f InstallerAuto.php rootpass=howdy login=openemr2 pass=hey dbname=openemr2 iuser=tom iuname=Miller iuserpass=heynow
- *     5) Create mutli-site (note this is very advanced usage)
- *          a. First create first installation
- *            php -f InstallerAuto.php
- *          b. Can create an installation that duplicates 'default' site but not the database
- *            php -f InstallerAuto.php login=openemr2 pass=openemr2 dbname=openemr2 site=default2 source_site_id=default
- *          c. Or can create an installation that duplicates 'default' site and database
- *             php -f InstallerAuto.php login=openemr2 pass=openemr2 dbname=openemr2 site=default2 source_site_id=default clone_database=yes
- *          d. Can continue installing new instances as needed ...
- *             php -f InstallerAuto.php login=openemr3 pass=openemr3 dbname=openemr3 site=default3 source_site_id=default clone_database=yes
- *     6) Provide pre-created database and restricted privilege user access credentials - example from Planettel.com.sg Proxmox OpenVZ Template
- *        (otherwise use default configuration settings - do not use for cloning / migration)
- *          php -f /var/www/openemr/contrib/util/installScripts/InstallerAuto.php no_root_db_access=1 iuserpass=oemr123 login=oemrusr pass=${UPASSWD} > /dev/null 2>&1
+ * Examples:
+ *   1) Install using default configuration settings
+ *      php InstallerAuto.php
+ *   2) Provide root sql user password
+ *      php InstallerAuto.php --rootpass=howdy
+ *   3) Provide root and openemr sql user passwords
+ *      php InstallerAuto.php --rootpass=howdy --pass=hey
+ *   4) Provide sql user settings and openemr user settings
+ *      php InstallerAuto.php --rootpass=howdy --login=openemr2 --pass=hey --dbname=openemr2 --iuser=tom --iuname=Miller --iuserpass=heynow
+ *   5) Create multi-site
+ *      a. First create first installation
+ *         php InstallerAuto.php
+ *      b. Create duplicate of 'default' site (no database clone)
+ *         php InstallerAuto.php --login=openemr2 --pass=openemr2 --dbname=openemr2 --site=default2 --source_site_id=default
+ *      c. Create duplicate of 'default' site with database clone
+ *         php InstallerAuto.php --login=openemr2 --pass=openemr2 --dbname=openemr2 --site=default2 --source_site_id=default --clone_database=yes
  *
  * @package   OpenEMR
  * @link      http://www.open-emr.org
@@ -74,60 +54,124 @@
  * @license   https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
  */
 
-// This exit is to avoid malicious use of this script.
-exit;
+// Check if script is being run from command line
+if (php_sapi_name() !== 'cli') {
+    throw new RuntimeException('This script can only be run from the command line.');
+}
 
 // Include standard libraries/classes
-require_once dirname(__FILE__) . '/../../../vendor/autoload.php';
+require_once __DIR__ . '/../../../vendor/autoload.php';
 
-// Set up default configuration settings
-$installSettings = array();
-$installSettings['iuser']                    = 'admin';
-$installSettings['iuname']                   = 'Administrator';
-$installSettings['iuserpass']                = 'pass';
-$installSettings['igroup']                   = 'Default';
-$installSettings['server']                   = 'localhost'; // mysql server
-$installSettings['loginhost']                = 'localhost'; // php/apache server
-$installSettings['port']                     = '3306';
-$installSettings['root']                     = 'root';
-$installSettings['rootpass']                 = 'BLANK';
-$installSettings['login']                    = 'openemr';
-$installSettings['pass']                     = 'openemr';
-$installSettings['dbname']                   = 'openemr';
-$installSettings['collate']                  = 'utf8mb4_general_ci';
-$installSettings['site']                     = 'default';
-$installSettings['source_site_id']           = 'BLANK';
-$installSettings['clone_database']           = 'BLANK';
-$installSettings['no_root_db_access']        = 'BLANK';
-$installSettings['development_translations'] = 'BLANK';
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
 
-// Collect parameters(if exist) for installation configuration settings
-for ($i = 1; $i < count($argv); $i++) {
-    $indexandvalue = explode("=", $argv[$i]);
-    $index = $indexandvalue[0];
-    $value = $indexandvalue[1];
-    $installSettings[$index] = $value;
-}
+/**
+ * Command to install and configure OpenEMR
+ */
+class OpenEMRInstallCommand extends Command
+{
+    protected static $defaultName = 'openemr:install';
+    protected static $defaultDescription = 'Install and configure OpenEMR';
 
-// Convert BLANK settings to empty
-$tempInstallSettings = array();
-foreach ($installSettings as $setting => $value) {
-    if ($value == "BLANK") {
-        $value = '';
+    /**
+     * Configure the command options
+     */
+    protected function configure()
+    {
+        $this
+            ->setDescription('Automatically install and configure OpenEMR')
+            ->setHelp('This command allows you to install and configure OpenEMR without using the web interface.')
+            ->addOption('iuser', null, InputOption::VALUE_REQUIRED, 'Initial user login name', 'admin')
+            ->addOption('iuname', null, InputOption::VALUE_REQUIRED, 'Initial user last name', 'Administrator')
+            ->addOption('iuserpass', null, InputOption::VALUE_REQUIRED, 'Initial user password', 'pass')
+            ->addOption('igroup', null, InputOption::VALUE_REQUIRED, 'Practice group name', 'Default')
+            ->addOption('server', null, InputOption::VALUE_REQUIRED, 'MySQL server', 'localhost')
+            ->addOption('loginhost', null, InputOption::VALUE_REQUIRED, 'php/apache server', 'localhost')
+            ->addOption('port', null, InputOption::VALUE_REQUIRED, 'MySQL port', '3306')
+            ->addOption('root', null, InputOption::VALUE_REQUIRED, 'MySQL server root username', 'root')
+            ->addOption('rootpass', null, InputOption::VALUE_REQUIRED, 'MySQL server root password', '')
+            ->addOption('login', null, InputOption::VALUE_REQUIRED, 'Username to MySQL openemr database', 'openemr')
+            ->addOption('pass', null, InputOption::VALUE_REQUIRED, 'Password to MySQL openemr database', 'openemr')
+            ->addOption('dbname', null, InputOption::VALUE_REQUIRED, 'MySQL openemr database name', 'openemr')
+            ->addOption('collate', null, InputOption::VALUE_REQUIRED, 'Collation for mysql', 'utf8mb4_general_ci')
+            ->addOption('site', null, InputOption::VALUE_REQUIRED, 'Location of this instance in sites/', 'default')
+            ->addOption('source_site_id', null, InputOption::VALUE_REQUIRED, 'Location of instance to clone and mirror', '')
+            ->addOption('clone_database', null, InputOption::VALUE_REQUIRED, 'If set, will clone database from source_site_id', '')
+            ->addOption('no_root_db_access', null, InputOption::VALUE_REQUIRED, 'Use pre-created database and user', '')
+            ->addOption('development_translations', null, InputOption::VALUE_REQUIRED, 'Use development translations', '');
     }
 
-    $tempInstallSettings[$setting] = $value;
+    /**
+     * Execute the command
+     *
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return int
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        // Create a SymfonyStyle object for nicer rendering
+        $io = new SymfonyStyle($input, $output);
+        $io->title('OpenEMR Installation');
+
+        // Set up the installation settings from command options
+        $installSettings = [
+            'iuser' => $input->getOption('iuser'),
+            'iuname' => $input->getOption('iuname'),
+            'iuserpass' => $input->getOption('iuserpass'),
+            'igroup' => $input->getOption('igroup'),
+            'server' => $input->getOption('server'),
+            'loginhost' => $input->getOption('loginhost'),
+            'port' => $input->getOption('port'),
+            'root' => $input->getOption('root'),
+            'rootpass' => $input->getOption('rootpass'),
+            'login' => $input->getOption('login'),
+            'pass' => $input->getOption('pass'),
+            'dbname' => $input->getOption('dbname'),
+            'collate' => $input->getOption('collate'),
+            'site' => $input->getOption('site'),
+            'source_site_id' => $input->getOption('source_site_id'),
+            'clone_database' => $input->getOption('clone_database'),
+            'no_root_db_access' => $input->getOption('no_root_db_access'),
+            'development_translations' => $input->getOption('development_translations'),
+        ];
+
+        // Display installation settings if verbose
+        if ($output->isVerbose()) {
+            $io->section('Installation Settings');
+            $io->table(
+                ['Setting', 'Value'],
+                array_map(function ($k, $v) {
+                    return [$k, $v ?: '(empty)'];
+                }, array_keys($installSettings), array_values($installSettings))
+            );
+        }
+
+        try {
+            // Install and configure OpenEMR using the Installer class
+            $installer = new Installer($installSettings);
+
+            if (!$installer->quick_install()) {
+                $io->error('Installation failed: ' . $installer->error_message);
+                return Command::FAILURE;
+            }
+
+            $io->success($installer->debug_message);
+            return Command::SUCCESS;
+        } catch (\Exception $e) {
+            $io->error('Installation failed with exception: ' . $e->getMessage());
+            return Command::FAILURE;
+        }
+    }
 }
 
-$installSettings = $tempInstallSettings;
-
-
-// Install and configure OpenEMR using the Installer class
-$installer = new Installer($installSettings);
-if (! $installer->quick_install()) {
-  // Failed, report error
-    echo "ERROR: " . $installer->error_message . "\n";
-} else {
-  // Successful
-    echo $installer->debug_message . "\n";
-}
+// Create and configure the application
+$application = new Application('OpenEMR Installer', '1.0.0');
+$command = new OpenEMRInstallCommand();
+$application->add($command);
+$application->setDefaultCommand(OpenEMRInstallCommand::getDefaultName(), true);
+$application->run();
