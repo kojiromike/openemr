c07bf9740a55310010ae2c056067406b982301fb
Michael A. Smith <michael@opencoreemr.com>
2025-12-24 11:46:09 -0500

    Test 1: only database.sql change

--- sql/database.sql
+++ sql/database.sql
@@ -15368,3 +15368,4 @@ CREATE TABLE `preference_value_sets` (
     -- General Preferences
     INSERT INTO preference_value_sets(`loinc_code`,`answer_code`,`answer_system`,`answer_display`,`sort_order`,`active`) VALUES
     ('95541-9', 314433002, 'http://snomed.info/sct', 'Preference for health professional (finding)', 1, 1);
+ALTER TABLE users ADD COLUMN test_col INT;
